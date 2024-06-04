9d7f766139ff55c77b5f7580f8f30ec141c69353
amit <amit.prasad@learningsprial.co.in>
2024-06-04 22:52:42 +0530

    changes

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -5,9 +5,11 @@ namespace App\Http\Resources;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class TaskResource extends JsonResource
 {
+    public static $wrap = false;
     /**
      * Transform the resource into an array.
      *
@@ -24,7 +26,7 @@ class TaskResource extends JsonResource
             'status' => $this->status,
             'priority' => $this->priority,
             'image_path' => $this->image_path && !(str_starts_with($this->image_path, 'http')) ?
-                Storage::url($this->image_path) : '',
+                Storage::url($this->image_path) : $this->image_path,
             'project_id' => $this->project_id,
             'project' => new ProjectResource($this->project),
             'assigned_user_id' => $this->assigned_user_id,
