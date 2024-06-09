33bbe0dcef5d7709e59679c25571f33b843bac9b
amit <amit.prasad@learningsprial.co.in>
2024-06-09 17:08:48 +0530

    changes

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class UserResource extends JsonResource
 {
+    public static $wrap = false;
     /**
      * Transform the resource into an array.
      *
@@ -16,8 +19,14 @@ class UserResource extends JsonResource
     {
         return [
             "id" => $this->id,
+            "avatar_url"=>$this->avatar? Storage::url($this->avatar) : null,
             "name" => $this->name,
-            "email" => $this->email
+            "email" => $this->email,
+            'created_at' => (new Carbon($this->created_at))->format('Y-m-d'),
+            'updated_at' => (new Carbon($this->updated_at))->format('Y-m-d'),
+            'is_admin' => (bool) $this->is_admin,
+            'last_message'=>$this->last_message,
+            'last_message_date'=>$this->last_message_date
         ];
     }
 }
