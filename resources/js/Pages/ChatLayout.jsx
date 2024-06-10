f6cc44b74ac900c88e16fa34aec95dbf1948bc35
amitprasad2007 <amit.prasad2007@rediffmail.com>
2024-06-10 18:50:49 +0530

    changes

--- resources/js/Pages/ChatLayout.jsx
+++ resources/js/Pages/ChatLayout.jsx
@@ -1,20 +1,40 @@
 import {usePage} from "@inertiajs/react";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function ChatLayout({children}){
     const page =usePage();
     const conversations = page.props.conversations;
     const selectedConversation = page.props.selectedConversation;
+    const [localConversation,setLocalConversation] =useState([]);
+    const [sortedConversation,setSortedConversation] =useState([]);
+    const [onlineUsers,setOnlineUsers]=useState({});
+    const isUserOnline = (userId)=>onlineUsers[userId];
+    useEffect( ()=>{
+        setLocalConversation(conversations);
+    },[conversations]);
     useEffect(() => {
         Echo.join("online")
             .here( (users)=>{
-                console.log("here",users);
+                const onlineUserObj =Object.fromEntries(
+                    users.map( (user)=>[user.id,user])
+                );
+                setOnlineUsers((pervOnlieUsers)=>{
+                    return {...pervOnlieUsers,...onlineUserObj};
+                });
             })
             .joining((user)=>{
-                console.log("joining",user);
+               setOnlineUsers((pervOnlieUsers)=>{
+                   const updatedUsers ={...pervOnlieUsers}
+                   updatedUsers[user.id]=user;
+                   return updatedUsers
+               });
             })
             .leaving((user)=>{
-                console.log("leaving",user);
+                setOnlineUsers((pervOnlieUsers)=>{
+                    const updatedUsers ={...pervOnlieUsers}
+                   delete updatedUsers[user.id];
+                    return updatedUsers
+                });
             })
             .error((error)=>{
                 console.log("erro",error);
