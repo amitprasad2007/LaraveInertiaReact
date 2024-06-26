db478017c9e4e1eba637aa871b93acf88f43de45
amitprasad2007 <amit.prasad2007@rediffmail.com>
2024-06-26 18:57:13 +0530

    changes

--- resources/js/Components/App/NewMessageInput.jsx
+++ resources/js/Components/App/NewMessageInput.jsx
@@ -20,8 +20,20 @@ export default function NewMessageInput({value,onChange,onSend}){
             input.current.style.height= "auto";
             input.current.style.height =input.current.scrollHeight + 1 +"px";
         },100)
-    }
+    };
+    useEffect(()=>{
+        adjustHeight();
+    },[value])
     return(
-        <></>
+        <textarea
+            ref={input}
+            value={value}
+            rows ="1"
+            placeholder="Tyep a Message"
+            onKeyDown={onInputKeyDown}
+            onChange={(ev)=>onChangeEvent(ev)}
+            className="input input-bordered w-full rounded-r-none resize-none overflow-y-auto max-h-40"
+        >
+        </textarea>
         );
 }
